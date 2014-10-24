e9ce81734ab7e9fe933bba95bfd09531beb6ddef
Jamie <jxberc@gmail.com>
2014-10-24 18:02:36 -0400

    add scrabble solution

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,35 +1,27 @@
 class Scrabble
 
+  SCORING = {"a"=>1, "e"=>1, "i"=>1, "o"=>1, "u"=>1, "l"=>1, "n"=>1, "r"=>1, 
+             "s"=>1, "t"=>1, "d"=>2, "g"=>2, "b"=>3, "c"=>3, "m"=>3, "p"=>3, 
+             "f"=>4, "h"=>4, "v"=>4, "w"=>4, "y"=>4, "k"=>5, "j"=>8, "x"=>8, 
+             "q"=>10, "z"=>10 }
 
-
-
-  def initialize word
-    @word = word
+  def initialize(word)
+    @word       = word
+    @valid_word = format(word)
   end
 
+  # convenient scoring
+  def self.score(word)
+    new(word).score
+  end
 
+  def score
+    @valid_word.chars.inject(0) { |sum,char| sum + SCORING[char] }
+  end
 
+  private
 
+  def format(word)
+    word.nil? ? "" : word.gsub(/\W/,"").downcase
+  end
 end
-
-points = [
-  [ %w(A E I O U L N R S T), 1 ],
-  [ %w(D G), 2 ],
-  [ %w(B C M P), 3 ],
-  [ %w(F H V W Y),4 ],
-  [ %w(K),5 ],
-  [ %w(J X), 8 ],
-  [ %w(Q Z), 10 ]
-  ]
-
-SCORES = points.inject({}) do |h, i|
-      letters = i[0]
-      points  = i[1]
-      ary = letters.zip [points] * letters.size
-      h.merge!(Hash[ary])
-    end
-
-SCORING = {"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, 
-           "S"=>1, "T"=>1, "D"=>2, "G"=>2, "B"=>3, "C"=>3, "M"=>3, "P"=>3, 
-           "F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J"=>8, "X"=>8, 
-           "Q"=>10, "Z"=>10}
