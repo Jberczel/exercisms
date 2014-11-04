791bc2540ef6eafb962ae505a40bfa98fdde57ab
Jamie <jxberc@gmail.com>
2014-11-04 12:15:52 -0500

    add secret handshake 2nd iteration

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -1,27 +1,29 @@
 class SecretHandshake
   attr_reader :num, :binary
-  COMMANDS = [10000, 1000, 100, 10, 1].zip([nil] + %w(jump close\ your\ eyes double\ blink wink)) 
 
-  def initialize(num)
-    @num = num
-    @binary = num.kind_of?(Integer) ? num.to_s(2).to_i : 0
+  binary =  [10000, 1000, 100, 10, 1]
+  commands =%w(reverse jump close\ your\ eyes double\ blink wink)
+  SECRETS = binary.zip commands
+
+  def initialize(input)
+    @num = input.kind_of?(Integer) ? input : 0
   end
 
-  def get_commands
-    tmp = binary
-    ary = []
-    COMMANDS.each do |limit, command|
-      next if limit > tmp
-      tmp = tmp % limit
-      ary.unshift command
-    end
-    ary.compact!
-    binary >= 1000 ? ary.reverse : ary
+  def binary
+    num.to_s(2).to_i
   end
 
   def commands
     @commands ||= get_commands
   end
-end
 
-p SecretHandshake.new("pig").commands
+  def get_commands
+    ary, tmp = [], binary
+    SECRETS.each do |limit, command|
+      next if limit > tmp
+      tmp = tmp % limit
+      ary.unshift command unless command == 'reverse'
+    end
+    binary >= 1000 ? ary.reverse : ary
+  end
+end
